9a2c3bcd23618b7dda91b488a50c32e5e6d0e9e1
yejiayu <yejiayu.fe@gmail.com>
2018-07-19 14:14:51 +0800

    feat: Add log package

---
+++ log/log.go
@@ -0,0 +1,84 @@
+package log
+
+import (
+	"os"
+	"strings"
+
+	logging "github.com/sirupsen/logrus"
+)
+
+var level = strings.ToLower(os.Getenv("LOG_LEVEL"))
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	logging.SetFormatter(&logging.TextFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	logging.SetOutput(os.Stdout)
+
+	switch level {
+	case "panic":
+		logging.SetLevel(logging.PanicLevel)
+	case "fatal":
+		logging.SetLevel(logging.FatalLevel)
+	case "error":
+		logging.SetLevel(logging.ErrorLevel)
+	case "warn":
+		logging.SetLevel(logging.WarnLevel)
+	case "info":
+		logging.SetLevel(logging.InfoLevel)
+	default:
+		logging.SetLevel(logging.DebugLevel)
+	}
+}
+
+// Info logs to the INFO log.
+// Arguments are handled in the manner of fmt.Print; a newline is appended if missing.
+func Info(args ...interface{}) {
+	logging.Info(args)
+}
+
+// Infof logs to the INFO log.
+// Arguments are handled in the manner of fmt.Printf; a newline is appended if missing.
+func Infof(format string, args ...interface{}) {
+	logging.Infof(format, args...)
+}
+
+// Warning logs to the WARNING and INFO logs.
+// Arguments are handled in the manner of fmt.Print; a newline is appended if missing.
+func Warning(args ...interface{}) {
+	logging.Warning(args...)
+}
+
+// Warningf logs to the WARNING and INFO logs.
+// Arguments are handled in the manner of fmt.Printf; a newline is appended if missing.
+func Warningf(format string, args ...interface{}) {
+	logging.Warningf(format, args...)
+}
+
+// Error logs to the ERROR, WARNING, and INFO logs.
+// Arguments are handled in the manner of fmt.Print; a newline is appended if missing.
+func Error(args ...interface{}) {
+	logging.Error(args...)
+}
+
+// Errorf logs to the ERROR, WARNING, and INFO logs.
+// Arguments are handled in the manner of fmt.Printf; a newline is appended if missing.
+func Errorf(format string, args ...interface{}) {
+	logging.Errorf(format, args...)
+}
+
+// Fatal logs to the FATAL, ERROR, WARNING, and INFO logs,
+// including a stack trace of all running goroutines, then calls os.Exit(255).
+// Arguments are handled in the manner of fmt.Print; a newline is appended if missing.
+func Fatal(args ...interface{}) {
+	logging.Fatal(args...)
+}
+
+// Fatalf logs to the FATAL, ERROR, WARNING, and INFO logs,
+// including a stack trace of all running goroutines, then calls os.Exit(255).
+// Arguments are handled in the manner of fmt.Printf; a newline is appended if missing.
+func Fatalf(format string, args ...interface{}) {
+	logging.Fatalf(format, args...)
+}
