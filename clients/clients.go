270aef5e11abe40a3068bbd462b2de6d0466ac34
yejiayu <yejiayu.fe@gmail.com>
2018-08-20 18:55:47 +0800

    feat: Add gRPC clients

---
+++ clients/clients.go
@@ -0,0 +1,35 @@
+package clients
+
+import (
+	"google.golang.org/grpc"
+
+	"github.com/yejiayu/go-cita/log"
+	"github.com/yejiayu/go-cita/pb"
+)
+
+func NewAuthClient(url string) pb.AuthClient {
+	conn, err := grpc.Dial(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return pb.NewAuthClient(conn)
+}
+
+func NewChainClient(url string) pb.ChainClient {
+	conn, err := grpc.Dial(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return pb.NewChainClient(conn)
+}
+
+func NewNetworkClient(url string) pb.NetworkClient {
+	conn, err := grpc.Dial(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return pb.NewNetworkClient(conn)
+}
